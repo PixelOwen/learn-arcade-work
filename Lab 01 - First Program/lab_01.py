990d6b74138dd19b64b16ed4d66bab1f5cd382bb
Pixel Owen <darrenosharris@gmail.com>
2021-03-25 12:00:45 +1300

    Changed Variables in Password Creator

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -2,18 +2,26 @@ from easygui import *
 
 from random import *
 
-#variable
+# variable
 name = enterbox("What is Your Name", "Name Please")
 yoda_quit = 1
 password_creator_quit = 1
 random_island_quit = 1
 
-prg = choicebox("What program do you want to run?", "Program Picker", choices=["Yoda The Prophet", "Password Creator", "Random Island", "Quit"])
+prg = choicebox("What program do you want to run?", "Program Picker",
+                choices=["Yoda The Prophet", "Password Creator", "Random Island", "Quit"])
 
 while True:
     # Yoda Program
     if prg == "Yoda The Prophet":
-        yoda_answers = ["Train yourself to let go of everything you fear to lose.", "You must unlearn what you have learned.", "Difficult to see. Always in motion is the future...", "Through the Force, things you will see. Other places. The Future...the past. Old friends long gone.", "Always pass what you have learned.", "Looking? Found someone you have, eh?", "Yes, a Jedi's strength flows from the Force. but beware of the dark side.", "You will find only what you bring in.", "PATIENCE YOU MUST HAVE my young padawan.", "Do or do not. There is no try."]
+        yoda_answers = ["Train yourself to let go of everything you fear to lose.",
+                        "You must unlearn what you have learned.",
+                        "Difficult to see. Always in motion is the future...",
+                        "Through the Force, things you will see. Other places. The Future...the past. Old friends long gone.",
+                        "Always pass what you have learned.", "Looking? Found someone you have, eh?",
+                        "Yes, a Jedi's strength flows from the Force. but beware of the dark side.",
+                        "You will find only what you bring in.", "PATIENCE YOU MUST HAVE my young padawan.",
+                        "Do or do not. There is no try."]
         while yoda_quit == 1:
             reply = choice(yoda_answers)
             enterbox("Question any you may ask young {}".format(name), "Yoda The Prophet")
@@ -23,11 +31,12 @@ while True:
     # Password creator
     elif prg == "Password Creator":
         msgbox("Welcome {} to Password Creator".format(name), "Password Creator")
-        noun = ["Cat", "Dog", "Jack", "Pony", "Fireball", "Car", "Fairy", "H"]
-        adjective = ["Shiny", "Smelly", "Hot", "Cold", "W", "Dark", "Fluffy"]
+        noun = ["X-Wing", "Fighter_Jet", "Moonlit_Valley", "Rocky_Concave", "Jeff_Bezos", "Crystal_Heart"]
+        adjective = ["Determined", "Strong", "Devious", "Lively", "Mischievous", "Defiant"]
         while password_creator_quit:
-            number = [randrange(0, 99), "36", "85", "79", "54", "74", "85", "Y"]
-            msgbox("Your New Password is:\n{}{}{}".format(choice(adjective), choice(noun), choice(number)), "Password Creator")
+            number = ["1", "2", "3", "5", "7", "11", "13", "17", "19", "23", "29", "31", "37", "41", "42", "47", "53", "59", "61", "67", "71", "73", "79", "83", "89", "97"]
+            msgbox("Your New Password is:\n{}_{}_{}".format(choice(adjective), choice(noun), choice(number)),
+                   "Password Creator")
             password_creator_quit = ynbox("Would You Like A Different Password?")
 
     # Random Island Program
@@ -36,15 +45,22 @@ while True:
         male = ["a Dog", "Jerry", "a very Muscular Centaur", "a Lizardman", "a Goldfish"]
         place = ["At Home", "on Pluto", "On the Dinosaur", "In the Giant Ape", "At Work"]
         female_wore = ["a brown cape", "a red tophat", "a green tuxedo", "a dead lion(unskinned)", "a yellow beanie"]
-        male_wore = ["a pink tutu dress", "a sparkly purple dress", "a bright yellow flamingo top hat", "a beautiful white wedding dress", "a purple skirt"]
+        male_wore = ["a pink tutu dress", "a sparkly purple dress", "a bright yellow flamingo top hat",
+                     "a beautiful white wedding dress", "a purple skirt"]
         female_said = ["what am I", "what are you", "how does one fly", "do birds have baths?", "Yay"]
-        male_said = ["love is life,life is love", "i love beans", "this macaroni is good", "kono dio da", "CHIPS PLEASE!!"]
-        result = ["a JoJo's Reference", "Saten Attac 4", "Zombie Apocalypse", "Lies", "the government being overthrown", "CHIPS AND YAY"]
-        planet_said = ["DIE DIE DIE", "NANI!!", "Oh noo, we ran out of milk", "oh look meteors", "im pretty sure the suns getting closer"]
+        male_said = ["love is life,life is love", "i love beans", "this macaroni is good", "kono dio da",
+                     "CHIPS PLEASE!!"]
+        result = ["a JoJo's Reference", "Saten Attac 4", "Zombie Apocalypse", "Lies", "the government being overthrown",
+                  "CHIPS AND YAY"]
+        planet_said = ["DIE DIE DIE", "NANI!!", "Oh noo, we ran out of milk", "oh look meteors",
+                       "im pretty sure the suns getting closer"]
         msgbox("Welcome {} to Random Island".format(name), "Random Island")
         while random_island_quit == 1:
-            msgbox("{} met {} {}. She was wearing {} and he was wearing {}".format(choice(female), choice(male), choice(place), choice(female_wore), choice(male_wore)), "Random Island")
-            msgbox("The female said {}, the man responded {}, the result was {}, and the planet said {}.".format( choice(female_said), choice(male_said), choice(result), choice(planet_said)), "Random Island")
+            msgbox("{} met {} {}. She was wearing {} and he was wearing {}".format(choice(female), choice(male),
+                                                                                   choice(place), choice(female_wore),
+                                                                                   choice(male_wore)), "Random Island")
+            msgbox("The female said {}, the man responded {}, the result was {}, and the planet said {}.".format(
+                choice(female_said), choice(male_said), choice(result), choice(planet_said)), "Random Island")
             random_island_quit = ynbox("Would You Like Another Random Island?", "Random Island")
 
     # quit function
@@ -53,9 +69,10 @@ while True:
 
     another_program = ynbox("Would you like to run another program?", "Program Picker")
     if another_program:
-        prg = choicebox("What program do you want to run?", "Program Picker", choices=["Yoda The Prophet", "Password Creator", "Random Island", "Quit"])
+        prg = choicebox("What program do you want to run?", "Program Picker",
+                        choices=["Yoda The Prophet", "Password Creator", "Random Island", "Quit"])
     else:
         break
 msgbox("Closing Program Now...", "Goodbye")
 # program ends
-# exit("Goodbye")
+exit("Goodbye")
